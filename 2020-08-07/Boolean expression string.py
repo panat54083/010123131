245110851a588e012e5f6bf84ab89e6c4320ef9b
panat54083 <68013322+panat54083@users.noreply.github.com>
2020-08-18 01:57:54 +0700

    Update Boolean expression string.py

--- 2020-08-07/Boolean expression string.py
+++ 2020-08-07/Boolean expression string.py
@@ -1,6 +1,6 @@
-# 6201012610052 Assingment สำหรับ Boolean expression string ในข้อที่ 1
+# 6201012610052 Assingment สำหรับ Boolean expression string ในข้อที่ 1 และ 2
 # PyGame version: 1.9.6
-#--------------------------Complete----------------------------
+#------------------------Uncomplete----------------------------
 # 17.50 07/08/2020 แก้ไขเล็กน้อย
 # 01.55 08/08/2020 แก้ไขการแปลงเป็น postfix
 # 16.56 08/08/2020 เพิ่ม expression tree และแก้ไข postfix
@@ -8,29 +8,39 @@
 # 5.00 09/08/2020 แก้ไขการจัดตำแหน่งในการวาด  Tree (ยังไม่สามารถแก้ปัญหาตัวอักษรกระพริบได้
 # 13.07 12/08/2020 update version pygame แก้ไขให้โค้ดสามารถเช้ากับ version pygame 1.9.6 (ก่อนหน้าเป็น 2.0.0 dev10)
 # 1.31 13/08/2020 แก้ไขอักษรกระพริบ
+# 1.53 18/08/2020 เพิ่มการสร้างตาราง หมายเหตุ ยังไม่ได้สรา้งให้สามารถรับบ input จาก .txt และ output ออกไปใน .txt
 
 import pygame
 import math
+from itertools import product
 class boolExpStr():
     
     def __init__(self, equation = None):
-        self.equa = equation.replace(" ","")
+        self.equa = equation.replace(" ","") # ตัดวงเล็บ
 
-    def findVariable(self):
+    # show input
+    def stringOfEqua(self): 
+        stringOfEqua = ''
+        for string in self.equa:
+            stringOfEqua += string
+        return stringOfEqua
+
+    # find variables in equation
+    def findVariable(self): 
         lista = list()
         for ela in self.putInList(self.equa):
-            if ela in ['I0', 'I1', 'I2', '0', '1']:
+            if ela in ['I0', 'I1', 'I2','I3']:
                 lista.append(ela)
+        return sorted(list(set(lista)))
 
-        return list(set(lista))
-
-#checkOperator        
+    # checkOperator        
     def isOperator(self, c):
         if c == '+' or c == '&' or c == '!':
             return True
         else:
             return False
-#นำสมการใส่เข้าไปใน list 
+
+    #put equation into list 
     def putInList(self,equa): 
         SYMBOLS = set('+&!()')
 
@@ -47,13 +57,13 @@ class boolExpStr():
             result.append(equa[mark:len(equa)])
 
         return result
-#Postfix 
-    # สร้างเพื่อจะนำไปทำเป็น Tree 
+
+    # make the equation to be postfix for creating expression Tree 
     def convertPost(self):
         equa = self.putInList(self.equa)
         openC = 0
 
-        for ea in equa:
+        for ea in equa: # Check if equation have wrong barcket
             if ea == '(':
                 openC += 1
             elif ea == ')':
@@ -65,7 +75,6 @@ class boolExpStr():
             elif openC < 0:
                 return "too ')' "
 
-        # print(equa)
         alphabet = ['0', '1', 'I1', 'I2','I3','I0']
         priority = {'+':1, '&':1, '!':2, '(':0}
         postfix = []
@@ -88,10 +97,11 @@ class boolExpStr():
         while len(stack) != 0:
             postfix.append(stack.pop())
         return postfix
-#data   
+
+    # Check data of Equation    
     def dataOfEquation(self):
         #==================================
-        print('-------------------------------------------')
+        print('------------------------------------------------------------')
         word1 = ''
         word2 = ''
         for n in self.putInList(self.equa):
@@ -100,57 +110,69 @@ class boolExpStr():
         for n in self.convertPost():
             word2 += n + ' '
         print('Postfix = ',word2)
-        # print(self.convertPost())
         #==================================
-        print('-------------------------------------------')
-        return None      
+        print('------------------------------------------------------------')
+        return None
+    
+    def createTable(self):
+        print('------------------------------------------------------------')
+        variables = self.findVariable()
+        num_variables = len(variables)
+        # nunberofrow = 2**num_variables
+        data_list = []
+        head = []
 
-#--------------------------------------------------------------------
-# ---------------------------PYGAME---------------------------------#
-#--------------------------------------------------------------------  
+        # สร้างหัวของตาราง
+        for var in variables:
+            head.append(var)
+        head.append(self.stringOfEqua()) #stringOfEqua คือสมการ
 
-pygame.init()
-pygame.display.set_caption('Expression Tree')
+        inputs = product([int(True), int(False)], repeat = num_variables)
+        for boolean in inputs:
+            data_list.append(list(boolean))
+        
+        
+        for l in range(len(data_list)):
+            new_equa = self.stringOfEqua()
+            for k in range(num_variables):
+                new_equa = new_equa.replace(variables[k],str(data_list[l][k])) # ทำการเปลี่ยนตัวแปรในสมการให้อยู่ในรูปของ T, F
+            # print(data_list[l])
+            # print(new_equa)
+            # ทำการใช้สมการใหม่ ในการหา คำตอบ
+            eachEqua = boolExpStr(new_equa)
+            root = expressionTree(eachEqua.convertPost())
+            result = evaluateExpressionTree(root)
+            data_list[l].append(result) # ใส่คำตอบ
 
-clock = pygame.time.Clock()
+        # สร้างหัวของตาราง
+        for f in head:
+            print(f, end = ' | ')
+        print('\n')   
+        new = ''
+        list_ = []
+        # สร้างข้อมูล Truth Table 
+        for i in range(len(data_list)):
+            for j in range(len(data_list[i])):
+                lenStr = len(head[j]) - len(str(int(data_list[i][j])))
+                left_lenStr = (lenStr)//2
+                right_lenStr = len(head[j]) - left_lenStr - len(str(int(data_list[i][j])))
+                new = ' '*left_lenStr + str(int(data_list[i][j])) + ' '*right_lenStr
+                print(new, end = ' | ')
+            print('\n')
+        print('------------------------------------------------------------')
+        return True
 
-scr_w, scr_h = 1000, 1000
-screen = pygame.display.set_mode((scr_w, scr_h))
-surface = pygame.Surface( screen.get_size(), pygame.SRCALPHA)
+#--------------------------------------------------------------------#
+#                    PYGAME EXPRESSION TREE                          #
+#--------------------------------------------------------------------#
 
-#--------------------------------------------------------------------
-BLUE = pygame.Color('#7FFFD4')
-BLACK = pygame.Color('#000000')
-radius = (scr_w**2 + scr_h**2)**(1/2)/40
-text_font_for_drawingtree = pygame.font.SysFont("leelawadeeui", int(radius*1.5))
-#--------------------------------------------------------------------
-class eT():
+class eT(): # Class for create Node
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
-
-def height(node):  #Ref https://www.geeksforgeeks.org/write-a-c-program-to-find-the-maximum-depth-or-height-of-a-tree/
-    if node is None: 
-        return 0 ;  
-    else : 
-
-        l_height = height(node.left) # ตรวจสอบทางซ้ายไปเรื่อยๆ
-        r_height = height(node.right) # ตรวจสอบทางขวาไปเรื่อยๆ
-  
-        # find which one larger
-        if (l_height > r_height): 
-            return l_height+1
-        else: 
-            return r_height+1
-
-def inorder(t): 
-	if t is not None: 
-		inorder(t.left) 
-		print(t.value,end ='')
-		inorder(t.right)
-
-def expressionTree(equation):        
+# Create Expression tree (input must be postfix form)
+def expressionTree(equation):  
     stack = []
     for ch in equation:
 
@@ -165,7 +187,7 @@ def expressionTree(equation):
             stack.append(t)
 
         else: # operator 
-            # ดึงเอาตัวอักษร
+            # pop operand
             t = eT(ch)
             t1 = stack.pop()
             t2 = stack.pop()
@@ -173,69 +195,93 @@ def expressionTree(equation):
             t.right = t1
             t.left = t2
 
-
             stack.append(t)
-
     t = stack.pop()          
 
-    return t      
+    return t
+
+# Find height of Expression Tree
+def height(node):  #Ref https://www.geeksforgeeks.org/write-a-c-program-to-find-the-maximum-depth-or-height-of-a-tree/
+    if node is None: 
+        return 0 ;  
+    else : 
+
+        l_height = height(node.left) # Keep checking the left
+        r_height = height(node.right) # Keep checking the right
+  
+        # find which one larger
+        if (l_height > r_height): 
+            return l_height+1
+        else: 
+            return r_height+1
+
+# Read in Inorder Form
+def inorder(node): 
+	if node is not None: 
+		inorder(node.left) 
+		print(node.value,end ='')
+		inorder(node.right)
+
+# Calculate Boolean
+def evaluateExpressionTree(root): # root ให้ใส่ expressionTree #Ref https://www.geeksforgeeks.org/evaluation-of-expression-tree/
+
+    if root is None:
+        return None
+    if root.left is None and root.right is None:
+        return root.value
+
+    # evaluate left tree     
+    left_eval = evaluateExpressionTree(root.left)
+	# evaluate right tree
+    right_eval = evaluateExpressionTree(root.right)
+
+    # Determine Logic
+    if root.value == '+':
+        return left_eval or right_eval
+    elif root.value == '&':
+        return left_eval and right_eval
+    elif root.value == '!':
+        return not right_eval
+
+# Drawing in Pygame
+# Drawing nodes
 def drawTree(node, x, y, dx, h): #ref https://gist.github.com/Liwink/b81e726ad89df8b0754a3a1d0c40d0b4
     if node is not None:
-
+        # Draw nodes
         pygame.draw.circle(surface, BLUE, (int(x), int(y)), int(radius))
         pygame.display.update()
-        #วาดเส้นเชื่อม
-        
-        if node.left is not None:
-            pygame.draw.line(surface,BLUE ,[int(x),int(y)],[int(x-dx),int(y+1/h*400)],2)
-        if node.right is not None:
-            pygame.draw.line(surface,BLUE ,[int(x),int(y)],[int(x+dx),int(y+1/h*400)],2)
-        
 
-        # วาด ฝั่งทางซ้าย และ ทางขวา 
-        drawTree(node.left, x-dx, y+1/h*400, dx/2, h)
-        drawTree(node.right, x+dx, y+1/h*400, dx/2, h)
-    
+        #Draw line
+        if node.left is not None:
+            #(x1, y1),(x2, y2)
+            pygame.draw.line(surface,BLUE ,[int(x),int(y)],[int(x-dx),int(y+1/h*300)],2)
+        if node.right is not None:
+            pygame.draw.line(surface,BLUE ,[int(x),int(y)],[int(x+dx),int(y+1/h*300)],2)
+        
+        # recursive until out of node or node is None
+        # Draw the left and right sides.
+        drawTree(node.left, x-dx, y+1/h*300, dx/2, h)
+        drawTree(node.right, x+dx, y+1/h*300, dx/2, h)
+        return True
+
+# Drawing Text
 def drawText(node, x, y, dx, h):
     if node :
+        # Draw text in each node
         text_surface = text_font_for_drawingtree.render(str(node.value), True , BLACK)
         text_rect = text_surface.get_rect()
         text_rect.center = (int(x), int(y))
+
         surface.blit(text_surface, (text_rect))
         pygame.display.update()
 
-        drawText(node.left, x-dx, y+1/h*400, dx/2, h)
-        drawText(node.right, x+dx, y+1/h*400, dx/2, h)
+        # recursive until out of node or node is None
+        # Draw the left and right sides.
+        drawText(node.left, x-dx, y+1/h*300, dx/2, h)
+        drawText(node.right, x+dx, y+1/h*300, dx/2, h)
+        return True
 
-#--------------------------------------------------------------------
-# Test
-#--------------------------------------------------------------------        
-eq1 = boolExpStr("(I0&I1 + !(I1&I2))")
-eq2 = boolExpStr("!(1+0)")
-eq3 = boolExpStr("!(!(0+I0&1))")
-eq4 = boolExpStr("(I0+!I1+!(I2))&(!I0+I1+I2)")
-eq5 = boolExpStr("!(I0&I1)+!(I1+I2)")
-eq6 = boolExpStr("(((I0&I1&!I2)+!I1)+I3)")
-
-#--------------------------------------------------------------------
-#ไว้ตรวจสอบรูปแบบของ Postfix
-data = eq1.dataOfEquation()
-
-# เปลี่ยน สมการ ได้ eq1-eq6
-anyeq = eq1.convertPost()
-node = expressionTree(anyeq)
-print('height of nodes is ',height(node))
-variable  = eq1.findVariable()
-print(eq1.findVariable())
-# inorder(node)
-#--------------------------------------------------------------------
-running = True
-fps = 60
-h = height(node)
-#--------------------------------------------------------------------
-
-drawTree(node, scr_w//2, (scr_h-100)//h, scr_w//math.log(h*15) ,h)
-      
+# main in Pygame
 def drawExpression():
     global running
 
@@ -246,16 +292,89 @@ def drawExpression():
             if event.type == pygame.QUIT:
                 pygame.image.save(screen, "screenshot.jpeg")
                 running = False
-
-        # node จุดแกน x จุดแกน y ตัวแปรเปลี่ยนแกน ความสูง
-            # pygame.display.update()
-        
-        drawText(node, int(scr_w//2), int((scr_h-100)//h), int(scr_w//math.log(h*15)) ,int(h))  
+            # press ESC
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.image.save(screen, "screenshot.jpeg")
+                    running = False
         
         screen.fill((255, 255, 255))
+        screen.blit(text, text_rect_label)
         screen.blit(surface, (0, 0))
         pygame.display.update()
-
+    
     pygame.quit()
 
+#-------------------------------------------------------------------#
+#                               Input                               #
+#-------------------------------------------------------------------#
+
+# equation
+listOfEquation = ["(I0&I1 + !(I1&I2))", "!(1+0)",
+                 "!(!(0+I0&1))", "(I0+!I1+!(I2))&(!I0+I1+I2)",
+                  "!(I0&I1)+!(I1+I2)", "(((I0&I1&!I2)+!I1)+I3)"]
+# N มี 1-6
+# N = 1  >>>  "(I0&I1 + !(I1&I2))"
+# N = 2  >>>  "!(1+0)"
+# N = 3  >>>  "!(!(0+I0&1))"
+# N = 4  >>>  "(I0+!I1+!(I2))&(!I0+I1+I2)"
+# N = 5  >>>  "!(I0&I1)+!(I1+I2)"
+# N = 6  >>>  "(((I0&I1&!I2)+!I1)+I3)"
+
+N = 6
+eq = boolExpStr(listOfEquation[N-1])
+#--------------------------------------------------------------------
+# main Variable
+anyeq = eq.convertPost() # Convert infix to postfix
+variable  = eq.findVariable() # variabel in equation
+node = expressionTree(anyeq) # Create Node from equation
+
+running = True
+fps = 60
+h = height(node)
+
+#--------------------------------------------------------------------
+# Cheack in foramtion of Equation
+eq.dataOfEquation()
+print('height of nodes is ',height(node))
+
+#--------------------------------------------------------------------
+# Result of create Table
+eq.createTable()
+
+#-------------------------------------------------------------------#
+#                               PYGAME                              #
+#-------------------------------------------------------------------#
+pygame.init()
+pygame.display.set_caption('Expression Tree')
+
+clock = pygame.time.Clock()
+
+scr_w, scr_h = 800, 600
+screen = pygame.display.set_mode((scr_w, scr_h))
+surface = pygame.Surface( screen.get_size(), pygame.SRCALPHA)
+
+#--------------------------------------------------------------------
+BLUE = pygame.Color('#7FFFD4')
+BLACK = pygame.Color('#000000')
+radius = (scr_w**2 + scr_h**2)**(1/2)/50
+text_font_for_drawingtree = pygame.font.SysFont("leelawadeeui", int(radius*1.5))
+text_font_for_label = pygame.font.SysFont("comicsansms", int(radius*2))
+
+#--------------------------------------------------------------------
+# Label
+text = text_font_for_label.render(str(eq.stringOfEqua()), True, BLACK)
+text_rect_label = text.get_rect()
+text_rect_label.center = (text.get_width()//2,text.get_height()//2)
+
+# x, y are position of picture and  dx is the variable that can change position
+# drawTree(node, x, y, dx, h)
+drawTree(node, scr_w//2, (scr_h-100)//h, scr_w//math.log(h*15) ,h)
+# drawText(node, x, y, dx, h)
+drawText(node, int(scr_w//2), int((scr_h-100)//h), int(scr_w//math.log(h*15)) ,int(h))  
+
+#--------------------------------------------------------------------
+# draw Expression Tree
 drawExpression()
+
+#--------------------------------------------------------------------
