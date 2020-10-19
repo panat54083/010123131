2d6eda1ad45feff2d27076eefc5ab2e7ea089759
panat54083 <68013322+panat54083@users.noreply.github.com>
2020-10-19 23:41:34 +0700

    Add files via upload

---
+++ Chat/SW.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'SW_Chat.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from SW2 import *
+
+
+class Ui_MainWindow1(object):
+    def connect(self):
+        self.window=QtWidgets.QMainWindow()
+        self.ui=Ui_MainWindow2()
+        self.ui.setupUi(self.window)
+        self.window.show()
+        # SW_Chat.hide()
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.setFixedSize(800, 540)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.Icon = QtWidgets.QLabel(self.centralwidget)
+        self.Icon.setGeometry(QtCore.QRect(260, 90, 251, 231))
+        self.Icon.setText("")
+        self.Icon.setPixmap(QtGui.QPixmap("mail-send.png"))
+        self.Icon.setScaledContents(True)
+        self.Icon.setOpenExternalLinks(False)
+        self.Icon.setObjectName("Icon")
+        self.EnterUsername = QtWidgets.QLabel(self.centralwidget)
+        self.EnterUsername.setGeometry(QtCore.QRect(80, 310, 311, 81))
+        self.EnterUsername.setObjectName("EnterUsername")
+        self.InputUser = QtWidgets.QLineEdit(self.centralwidget)
+        self.InputUser.setGeometry(QtCore.QRect(400, 320, 331, 61))
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        self.InputUser.setFont(font)
+        self.InputUser.setObjectName("InputUser")
+        self.Background = QtWidgets.QLabel(self.centralwidget)
+        self.Background.setGeometry(QtCore.QRect(0, 0, 800, 530))
+        self.Background.setText("")
+        self.Background.setPixmap(QtGui.QPixmap("Background.png"))
+        self.Background.setObjectName("Background")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(260, 20, 301, 141))
+        self.label.setObjectName("label")
+        self.connectButton = QtWidgets.QPushButton(self.centralwidget)
+        self.connectButton.setGeometry(QtCore.QRect(360, 410, 93, 28))
+        self.connectButton.setObjectName("ConnectButton")
+        self.connectButton.clicked.connect(self.connect)
+        self.Background.raise_()
+        self.Icon.raise_()
+        self.EnterUsername.raise_()
+        self.InputUser.raise_()
+        self.label.raise_()
+        self.connectButton.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("SW-Chat", "SW-Chat"))
+        self.EnterUsername.setText(_translate("MainWindow", "<html><head/><body><p><span style=\" font-size:24pt;\">Enter Username :</span></p></body></html>"))
+        self.InputUser.setText(_translate("MainWindow", ""))
+        self.label.setText(_translate("MainWindow", "<html><head/><body><p align=\"center\"><span style=\" font-size:26pt; font-weight:600;\">SW-CHAT</span></p></body></html>"))
+        self.connectButton.setText(_translate("MainWindow", "Connect"))
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    SW_Chat = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow1()
+    ui.setupUi(SW_Chat)
+    SW_Chat.show()
+    sys.exit(app.exec_())
+
---
+++ Chat/SW2.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'SW2.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow2(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 540)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(0, 0, 801, 521))
+        self.label.setText("")
+        self.label.setPixmap(QtGui.QPixmap("Background.png"))
+        self.label.setObjectName("label")
+        self.Disconnect = QtWidgets.QPushButton(self.centralwidget)
+        self.Disconnect.setGeometry(QtCore.QRect(70, 430, 93, 28))
+        self.Disconnect.setObjectName("Disconnect")
+        self.TypeHere = QtWidgets.QTextEdit(self.centralwidget)
+        self.TypeHere.setGeometry(QtCore.QRect(310, 340, 421, 71))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.TypeHere.setFont(font)
+        self.TypeHere.setObjectName("TypeHere")
+        self.Send = QtWidgets.QPushButton(self.centralwidget)
+        self.Send.setGeometry(QtCore.QRect(640, 430, 93, 28))
+        self.Send.setObjectName("Send")
+        self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser.setGeometry(QtCore.QRect(80, 20, 201, 51))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        font.setBold(True)
+        font.setWeight(75)
+        self.textBrowser.setFont(font)
+        self.textBrowser.setStyleSheet("border: 0;")
+        self.textBrowser.setObjectName("textBrowser")
+        self.Usernamedisplay = QtWidgets.QTextBrowser(self.centralwidget)
+        self.Usernamedisplay.setGeometry(QtCore.QRect(70, 350, 221, 51))
+        font = QtGui.QFont()
+        font.setPointSize(18)
+        self.Usernamedisplay.setFont(font)
+        self.Usernamedisplay.setStyleSheet("border: 0;")
+        self.Usernamedisplay.setObjectName("Usernamedisplay")
+        self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setGeometry(QtCore.QRect(70, 80, 211, 231))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.textBrowser_2.setFont(font)
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.textBrowser_3 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_3.setGeometry(QtCore.QRect(310, 20, 421, 291))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.textBrowser_3.setFont(font)
+        self.textBrowser_3.setObjectName("textBrowser_3")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.Disconnect.setText(_translate("MainWindow", "Disconnect"))
+        self.Send.setText(_translate("MainWindow", "Send"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:18pt; font-weight:600; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">Online User</span></p></body></html>"))
+        self.Usernamedisplay.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:18pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'MS Shell Dlg 2\';\">I Here Too</span></p></body></html>"))
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    SW_Chat = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow2()
+    ui.setupUi(SW_Chat)
+    SW_Chat.show()
+    sys.exit(app.exec_())
